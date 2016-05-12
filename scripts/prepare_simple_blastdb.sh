f2466e8d0f1d1eb258ce83d4fa986b4cee57d549
Atul Nag <nag.atul@gmail.com>
2016-05-12 19:55:08 +0530

    Add -hash_index

--- scripts/prepare_simple_blastdb.sh
+++ scripts/prepare_simple_blastdb.sh
@@ -6,7 +6,7 @@ sequence=$1
 dtype=$2
 
 #create BLAST database with masking information
-makeblastdb -in $sequence -dbtype ${dtype} -parse_seqids -out $sequence -logfile ${sequence}_blast.log
+makeblastdb -in $sequence -dbtype ${dtype} -parse_seqids -hash_index -out $sequence -logfile ${sequence}_blast.log
 
 #verify if masking information is added properly
 blastdbcmd -db $sequence -info
