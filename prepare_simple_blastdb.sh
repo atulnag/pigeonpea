a34962e9cf44c55cf08600683ad2a63af0ebcbfa
Atul Nag <nag.atul@gmail.com>
2016-05-12 19:41:16 +0530

    Simple BLAST database
    Script to create simple BLAST database with inputs as the name of file containing local sequences in FASTA format and the type of sequences contained.

---
+++ prepare_simple_blastdb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Phase 1
+
+sequence=$1
+dtype=$2
+
+#create BLAST database with masking information
+makeblastdb -in $sequence -dbtype ${dtype} -parse_seqids -out $sequence -logfile ${sequence}_blast.log
+
+#verify if masking information is added properly
+blastdbcmd -db $sequence -info
